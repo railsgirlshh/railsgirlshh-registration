22a8120c2437d44d70b0f386b0e0a0839440483d
titanoboa <dev@kpuschke.eu>
2014-06-08 00:21:32 +0200

    validations for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,34 @@
 class Event < ActiveRecord::Base
+  validates_presence_of :title, :event_date
+
+  validates_each :coach_reg_start, :coach_reg_end, :attendee_reg_start, :attendee_reg_end do |record, attr, value|
+    record.errors.add(attr, 'must be before event date') if value.present? && value > record.event_date
+  end
+
+  validate :coach_reg_end_after_start
+  validate :attendee_reg_end_after_start
+
+  validate :event_in_the_future, on: :create
+
+  private
+  def event_in_the_future
+    return if event_date.blank?
+    errors.add(:event_date, "must be in the future") if event_date < Date.today 
+  end
+
+  def coach_reg_end_after_start
+    return if coach_reg_end.blank? || coach_reg_start.blank?
+ 
+    if coach_reg_end < coach_reg_start
+      errors.add(:coach_reg_end, "must be after the coach reg start") 
+    end 
+  end
+
+  def attendee_reg_end_after_start
+    return if attendee_reg_end.blank? || attendee_reg_start.blank?
+ 
+    if attendee_reg_end < attendee_reg_start
+      errors.add(:attendee_reg_end, "must be after the attendee reg start") 
+    end 
+  end
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
+module EventTest
+  class EventValidationTest < ActiveSupport::TestCase
+    test "should save event with title and event date only" do
+      event = Event.new(title: 'Summer workshop', event_date: Date.today+5)
+      assert event.save, "Saved event with title and event date only"
+    end
 
-class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "should save event with full data" do
+      event = Event.new(title: 'Summer workshop', description: 'Most awesome workshop ever', event_date: Date.today+5, coach_reg_start: Date.today+3, coach_reg_end: Date.today+4, attendee_reg_start: Date.today+3, attendee_reg_end: Date.today+4)
+      assert event.save, "Saved event with title and event date only"
+    end
+
+    test "should not save event without title" do
+      event = Event.new(event_date: Date.today+2)
+      assert_not event.save, "Saved the event without a title"
+    end
+
+    test "should not save event without event date" do
+      event = Event.new(title: 'Summer workshop')
+      assert_not event.save, "Saved the event without an event date"
+    end
+
+    test "should not save event with attendee registration start after end" do
+      event = Event.new(title: 'Summer workshop', event_date: Date.today+5, attendee_reg_start: Date.today+4, attendee_reg_end: Date.today+3)
+      assert_not event.save, "Saved the event with attendee registration start after end"
+    end
+
+    test "should not save event with coach registration start after end"  do
+      event = Event.new(title: 'Summer workshop', event_date: Date.today+5, coach_reg_start: Date.today+4, coach_reg_end: Date.today+3)
+      assert_not event.save, "Saved the event with coach registration start after end"
+    end
+
+    test "should not create event with event date in the past"  do
+      event = Event.new(title: 'Summer workshop', event_date: Date.today-5)
+      assert_not event.save, "Created the event with event date in the past"
+    end
+
+    test "should update event with event date in the past"  do
+      event = Event.new(title: 'Summer workshop', event_date: Date.today+5)
+      event.save
+      event.event_date= Date.today-2
+      assert event.save, "Updated the event with event date in the past"
+    end
+  end
 end
