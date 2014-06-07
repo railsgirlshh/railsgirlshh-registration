0627b98f4db369841419a5b978fd810f5b0bf6a9
titanoboa <dev@kpuschke.eu>
2014-06-07 18:19:53 +0200

    event editing for admins only

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  resources :events
+  resources :events, only: [:index]
+
+  namespace :admin do
+    resources :events
+  end
+
+  get '/admin' => redirect('/admin/events')
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,49 +1,10 @@
 require 'test_helper'
 
 class EventsControllerTest < ActionController::TestCase
-  setup do
-    @event = events(:one)
-  end
 
   test "should get index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:events)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create event" do
-    assert_difference('Event.count') do
-      post :create, event: { attendee_reg_end: @event.attendee_reg_end, attendee_reg_start: @event.attendee_reg_start, coach_reg_end: @event.coach_reg_end, coach_reg_start: @event.coach_reg_start, description: @event.description, event_date: @event.event_date, title: @event.title }
-    end
-
-    assert_redirected_to event_path(assigns(:event))
-  end
-
-  test "should show event" do
-    get :show, id: @event
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @event
-    assert_response :success
-  end
-
-  test "should update event" do
-    patch :update, id: @event, event: { attendee_reg_end: @event.attendee_reg_end, attendee_reg_start: @event.attendee_reg_start, coach_reg_end: @event.coach_reg_end, coach_reg_start: @event.coach_reg_start, description: @event.description, event_date: @event.event_date, title: @event.title }
-    assert_redirected_to event_path(assigns(:event))
-  end
-
-  test "should destroy event" do
-    assert_difference('Event.count', -1) do
-      delete :destroy, id: @event
-    end
-
-    assert_redirected_to events_path
   end
 end
