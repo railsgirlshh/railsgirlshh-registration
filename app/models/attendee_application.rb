529711a3abbdda2515362cfecea1c02f63150efa
titanoboa <dev@kpuschke.eu>
2014-06-08 23:31:18 +0200

    validate presence of mandatory fields in attendee_application

--- app/models/attendee_application.rb
+++ app/models/attendee_application.rb
@@ -1,3 +1,6 @@
 class AttendeeApplication < ActiveRecord::Base
   belongs_to :event
+  
+  validates_presence_of :first_name, :last_name, :email, :event
+
 end
--- test/models/attendee_application_test.rb
+++ test/models/attendee_application_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
+module AttendeeApplicationTest
+  class AttendeeApplicationValidationTest < ActiveSupport::TestCase
+    setup do
+      @event = events(:minimal_event)
+    end
 
-class AttendeeApplicationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "should not save attendee application without first name" do
+      attendee_application = @event.attendee_applications.build(last_name: 'Foo', email: 'foo@example.com')
+      assert_not attendee_application.save, "Saved the attendee application without a first name"
+    end
+
+    test "should not save attendee application without last name" do
+      attendee_application = @event.attendee_applications.build(first_name: 'Foo', email: 'foo@example.com')
+      assert_not attendee_application.save, "Saved the attendee application without a last name"
+    end
+
+    test "should not save attendee application without email" do
+      attendee_application = @event.attendee_applications.build(first_name: 'Foo', last_name: 'bar')
+      assert_not attendee_application.save, "Saved the attendee application without email"
+    end
+
+    test "should not save attendee application without event association" do
+      attendee_application = AttendeeApplication.new(first_name: 'Foo', last_name: 'bar', email: 'foo@example.com')
+      assert_not attendee_application.save, "Saved the attendee application without event association"
+    end
+  end
 end
