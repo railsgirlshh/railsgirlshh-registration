bc49d3e85e77ef4dc751e6f5063b8c493c664670
Lieblingswelt <lieblingswelt@posteo.de>
2014-08-09 18:03:06 +0200

    adds translation for events_helper.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,21 +1,21 @@
 module EventsHelper
   def attendee_registration_display(event)
     if event.attendee_reg_open?
-      "Register for this event. Registration closes #{event.attendee_reg_end_date.to_s(:long)}."
+     "#{t('events_helper.registration_open')} #{event.attendee_reg_end_date.to_s(:long)}"
     elsif event.attendee_reg_end_date < Date.today
-      "Registration is closed."
+      "#{t('events_helper.registration_closed')}"
     elsif event.attendee_reg_start > Date.today
-      "Registration opens #{event.attendee_reg_start.to_s(:long)} and closes #{event.attendee_reg_end_date.to_s(:long)}."
+      "#{t('events_helper.registration_opens')} #{event.attendee_reg_start.to_s(:long)} #{t('events_helper.registration_closes')} #{event.attendee_reg_end_date.to_s(:long)}."
     end
   end
 
   def coach_registration_display(event)
     if event.coach_reg_open?
-      "Register for this event. Registration closes #{event.coach_reg_end_date.to_s(:long)}."
+      "#{t('events_helper.registration_open')} #{event.coach_reg_end_date.to_s(:long)}."
     elsif event.coach_reg_end_date < Date.today
-      "Registration is closed."
+      "#{t('events_helper.registration_closed')}"
     elsif event.coach_reg_start > Date.today
-      "Registration opens #{event.coach_reg_start.to_s(:long)} and closes #{event.coach_reg_end_date.to_s(:long)}."
+      "#{t('events_helper.registration_opens')} #{event.coach_reg_start.to_s(:long)} #{t('events_helper.registration_closes')} #{event.coach_reg_end_date.to_s(:long)}."
     end
   end
 end
