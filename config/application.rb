4849e27a30859140399672c6120b8f847db060cf
titanoboa <dev@kpuschke.eu>
2014-08-17 12:27:20 +0200

    mailer configuration

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,18 @@ module RailsgirlshhRegistration
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :de
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               ENV["MY_DOMAIN"] || 'gmail.com',
+      user_name:            'railsgirlshh@gmail.com',
+      password:             ENV["EMAIL_PASSWORD"],
+      authentication:       'plain',
+      enable_starttls_auto: true
+    }
+    config.action_mailer.default_options = { from: 'Rails Girls Hamburg <railsgirlshh@gmail.com>' }
+    config.action_mailer.default_url_options = { :host => ENV["MY_DOMAIN"] || 'localhost:3000'}
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,10 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
+  # if you want to send emails in the development environment
+  # set the following two settings to true
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
