a450595c97706b3efb23c144fdd88188a367b732
titanoboa <dev@kpuschke.eu>
2014-08-30 21:28:46 +0200

    send acceptance email

--- app/mailers/attendee_application_mailer.rb
+++ app/mailers/attendee_application_mailer.rb
@@ -5,4 +5,9 @@ class AttendeeApplicationMailer < ActionMailer::Base
     @attendee_application = attendee_application
     mail(to: @attendee_application.email, subject: 'Rails Girls Hamburg Workshop')
   end
+
+  def accepted_email(attendee_application)
+    @attendee_application = attendee_application
+    mail(to: @attendee_application.email, subject: 'Teilnahme am Rails Girls Hamburg Workshop')
+  end
 end
