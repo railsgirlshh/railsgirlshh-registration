9e953fea6c12fa5dcbcf3d650b9a8cb74cc5726f
titanoboa <dev@kpuschke.eu>
2014-08-30 22:25:02 +0200

    oops, was accidentally not checked in

--- test/controllers/coach_applications_controller_test.rb
+++ test/controllers/coach_applications_controller_test.rb
@@ -49,4 +49,15 @@ class CoachApplicationsControllerTest < ActionController::TestCase
 
     assert_redirected_to root_path
   end
+
+  test "should send welcome email when application is created" do
+    assert_difference 'ActionMailer::Base.deliveries.size', +1 do
+      post :create, event_id: @coach_application.event_id, coach_application: { application_text: @coach_application.application_text, email: @coach_application.email, event_id: @coach_application.event_id, first_name: @coach_application.first_name, last_name: @coach_application.last_name, other_text: @coach_application.other_text}
+    end
+    welcome_email = ActionMailer::Base.deliveries.last
+
+    assert_equal "Rails Girls Hamburg Workshop", welcome_email.subject
+    assert_equal @coach_application.email, welcome_email.to[0]
+    assert_match(/Hallo Grace/, welcome_email.body.to_s)
+  end
 end
