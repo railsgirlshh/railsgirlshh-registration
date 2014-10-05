bd1bb3a39ff77894d1b1af753cc3424bbbce0d35
titanoboa <dev@kpuschke.eu>
2014-10-05 23:36:48 +0200

    small improvements for events index page

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,21 +1,21 @@
 module EventsHelper
   def attendee_registration_display(event)
     if event.attendee_reg_open?
-     "#{t('events_helper.registration_open')} #{event.attendee_reg_end_date.to_s(:long)}"
+     t 'events_helper.attendee_registration_open', application_end_date: event.attendee_reg_end_date.to_s(:long)
     elsif event.attendee_reg_end_date < Date.today
-      "#{t('events_helper.registration_closed')}"
+      t 'events_helper.attendee_registration_closed'
     elsif event.attendee_reg_start > Date.today
-      "#{t('events_helper.registration_opens')} #{event.attendee_reg_start.to_s(:long)} #{t('events_helper.registration_closes')} #{event.attendee_reg_end_date.to_s(:long)}."
+      t 'events_helper.attendee_registration_opens', application_start_date: event.attendee_reg_start.to_s(:long), application_end_date: event.attendee_reg_end_date.to_s(:long)
     end
   end
 
   def coach_registration_display(event)
     if event.coach_reg_open?
-      "#{t('events_helper.registration_open')} #{event.coach_reg_end_date.to_s(:long)}."
+      t 'events_helper.coach_registration_open', registration_end_date: event.coach_reg_end_date.to_s(:long)
     elsif event.coach_reg_end_date < Date.today
-      "#{t('events_helper.registration_closed')}"
+      t 'events_helper.coach_registration_closed'
     elsif event.coach_reg_start > Date.today
-      "#{t('events_helper.registration_opens')} #{event.coach_reg_start.to_s(:long)} #{t('events_helper.registration_closes')} #{event.coach_reg_end_date.to_s(:long)}."
+      t 'events_helper.coach_registration_opens', registration_start_date: event.coach_reg_start.to_s(:long), registration_end_date: event.coach_reg_end_date.to_s(:long)
     end
   end
 end
