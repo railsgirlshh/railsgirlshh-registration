e8879daff9aa5f99f110fa30aa12ad8a8156de15
titanoboa <dev@kpuschke.eu>
2014-11-02 21:32:19 +0100

    localize date format

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,21 +1,21 @@
 module EventsHelper
   def attendee_registration_display(event)
     if event.attendee_reg_open?
-     t 'events_helper.attendee_registration_open', application_end_date: event.attendee_reg_end_date.to_s(:long)
+     t 'events_helper.attendee_registration_open', application_end_date: l(event.attendee_reg_end_date, format: :long)
     elsif event.attendee_reg_end_date < Date.today
       t 'events_helper.attendee_registration_closed'
     elsif event.attendee_reg_start > Date.today
-      t 'events_helper.attendee_registration_opens', application_start_date: event.attendee_reg_start.to_s(:long), application_end_date: event.attendee_reg_end_date.to_s(:long)
+      t 'events_helper.attendee_registration_opens', application_start_date: l(event.attendee_reg_start, format: :long), application_end_date: l(event.attendee_reg_end_date, format: :long)
     end
   end
 
   def coach_registration_display(event)
     if event.coach_reg_open?
-      t 'events_helper.coach_registration_open', registration_end_date: event.coach_reg_end_date.to_s(:long)
+      t 'events_helper.coach_registration_open', registration_end_date: l(event.coach_reg_end_date, format: :long)
     elsif event.coach_reg_end_date < Date.today
       t 'events_helper.coach_registration_closed'
     elsif event.coach_reg_start > Date.today
-      t 'events_helper.coach_registration_opens', registration_start_date: event.coach_reg_start.to_s(:long), registration_end_date: event.coach_reg_end_date.to_s(:long)
+      t 'events_helper.coach_registration_opens', registration_start_date: l(event.coach_reg_start, format: :long), registration_end_date: l(event.coach_reg_end_date, format: :long)
     end
   end
 end
