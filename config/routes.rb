96f2b2ec170e7191f77f8ad2cee76aba584c85f9
titanoboa <dev@kpuschke.eu>
2014-11-09 23:19:27 +0100

    Revert "remove catch-all route since it redirects requests to precompiled assets"
    This reverts commit b9231aaac61fd28d35174139c95db1af1cb3f98a.

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,8 @@ Rails.application.routes.draw do
 
   get '/admin' => redirect('/admin/events')
 
+  get '*path' => redirect('/') unless Rails.env.development?
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
