cce93f907575d156a6fda1f523713f7d5f53a0bb
sssggr <n.siessegger@zeromail.org>
2014-12-20 17:36:57 +0100

    replace coach application mail subjects with I18n keys

--- app/mailers/coach_application_mailer.rb
+++ app/mailers/coach_application_mailer.rb
@@ -4,6 +4,6 @@ class CoachApplicationMailer < ActionMailer::Base
 
   def welcome_email(coach_application)
     @coach_application = coach_application
-    mail(to: @coach_application.email, subject: 'Rails Girls Hamburg Workshop')
+    mail(to: @coach_application.email, subject: I18n.t('mailer.coach_application.welcome_email.subject'))
   end
 end
